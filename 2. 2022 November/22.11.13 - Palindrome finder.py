461816a1da7d2d2b44e4aef6d53d8afaae2670c7
abelkent <abel.wbkent@gmail.com>
2022-11-13 17:46:46 +0000

    Create file

---
+++ 2. 2022 November/22.11.13 - Palindrome finder.py
@@ -0,0 +1,60 @@
+""""
+Given a string, find the palindrome that can be made by inserting the fewest number of characters as possible anywhere in the word. 
+If there is more than one palindrome of minimum length that can be made, return the lexicographically earliest one (the first one alphabetically).
+
+For example, given the string "race", you should return "ecarace", since we can add three letters to it 
+(which is the smallest amount to make a palindrome). There are seven other palindromes that can be made from "race" 
+by adding three letters, but "ecarace" comes first alphabetically.
+
+As another example, given the string "google", you should return "elgoogle".
+"""
+
+import string
+
+def palidrome_generator(sequence):
+
+    #Gets alphabet
+    alphabet = list(string.ascii_uppercase)
+    #print(alphabet)
+
+    #Determine if palindrome
+    #Sets sequence to uppercase
+    sequence = sequence.upper()
+    sequence = [x for x in sequence]
+
+    backwards_sequence = list(sequence)
+    backwards_sequence.reverse()
+    
+    if backwards_sequence == sequence:
+        return ("INPUT IS ALREADY A PALINDROME")
+    
+
+    def forward_vs_backwards(forwards, backwards):
+        forward_index = alphabet.index(forwards[0])
+        backward_index = alphabet.index(backwards[0])
+
+        if forward_index < backward_index:
+            return "Forward"
+        
+        elif forward_index > backward_index:
+            return "Backwards"
+        
+        elif forward_index == backward_index:
+            return forward_vs_backwards(forwards[1:], backwards[1:])
+
+    direction = forward_vs_backwards(sequence, backwards_sequence)
+
+
+    print(direction)
+
+    if direction == "Forwards":
+        index = 0
+    elif direction == "Backwards":
+        index = -1
+    
+    print(sequence[index])
+
+
+
+test = palidrome_generator("google")
+#print(test)
