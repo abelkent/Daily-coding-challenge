6e9d7ca9adb11a7a31f4bdd23611e84c62dc3f4f
abelkent <abel.wbkent@gmail.com>
2023-01-09 16:38:32 +0000

    Create 08.01.2023 - Correcting faulty code (anonymous (lambda) functions).py
    - Initial commit
    - Discussion but incorrect at TOW

---
+++ 2023/1. January/08.01.2023 - Correcting faulty code (anonymous (lambda) functions).py
@@ -0,0 +1,19 @@
+"""
+What does the below code snippet print out? How can we fix the anonymous functions to behave as we'd expect?
+
+functions = []
+for i in range(10):
+    functions.append(lambda : i)
+
+for f in functions:
+    print(f())
+"""
+
+"""
+DISCUSSION
+Code snippet prints out ten instances of "9", as opposed to the (presumably intended) output of 0 - 9.
+
+Issue arises from how lambda calling of i makes every call of 9 call the final value of 9 rather than the leading values of 0-9.
+"""
+
+#AS OF YET CORRECTED
