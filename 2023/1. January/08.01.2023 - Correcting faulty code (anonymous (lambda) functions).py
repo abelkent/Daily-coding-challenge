f0b3fdd7f97feffe512369ff988aa094d840b8c9
abelkent <abel.wbkent@gmail.com>
2023-01-09 16:56:10 +0000

    Annotation update for my peace of mind

--- 2023/1. January/08.01.2023 - Correcting faulty code (anonymous (lambda) functions).py
+++ 2023/1. January/08.01.2023 - Correcting faulty code (anonymous (lambda) functions).py
@@ -16,4 +16,4 @@ Code snippet prints out ten instances of "9", as opposed to the (presumably inte
 Issue arises from how lambda calling of i makes every call of 9 call the final value of 9 rather than the leading values of 0-9.
 """
 
-#AS OF YET CORRECTED
+#NOT AS OF YET CORRECTED
