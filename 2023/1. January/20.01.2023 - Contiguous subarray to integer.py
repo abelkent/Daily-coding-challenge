4d13881fb250a867bf96d1db7af5d54d806be891
abelkent <abel.wbkent@gmail.com>
2023-01-21 19:15:40 +0000

    Annotation and minor tweak

--- 2023/1. January/20.01.2023 - Contiguous subarray to integer.py
+++ 2023/1. January/20.01.2023 - Contiguous subarray to integer.py
@@ -6,14 +6,17 @@ For example, if the list is [1, 2, 3, 4, 5] and K is 9, then it should return [2
 
 def contiguous_sum_finder(array, target):
 
+    #Initialises output array 
     output_array = list()
 
+    #Iterates through elements in array, adding them to output array
     for element in array:
         output_array.append(element)
-        if sum(output_array) > target:
-            while sum(output_array) > target:
-                output_array.pop(0)
+        #If sum of total list is greater than target, removes first element repeatedly until no longer satisfied
+        while sum(output_array) > target:
+            output_array.pop(0)
         
+        #If sum of elements is equal to target, prints and returns list
         if sum(output_array) == target:
             print(output_array)
             return output_array
