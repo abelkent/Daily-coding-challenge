0f5bb04b20e8ade3346919d94001f4ccab25337d
abelkent <abel.wbkent@gmail.com>
2023-01-23 21:40:05 +0000

    Create 22.01.2023 - Check if linked list is palindromic.py
    - Currently not functional

---
+++ 2023/1. January/22.01.2023 - Check if linked list is palindromic.py
@@ -0,0 +1,32 @@
+"""
+Determine whether a doubly linked list is a palindrome. What if it’s singly linked?
+
+For example, 1 -> 4 -> 3 -> 4 -> 1 returns True while 1 -> 4 returns False.
+"""
+
+#Linked list implementation sourced from: https://realpython.com/linked-lists-python/
+#Currently not working
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append(node.data)
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
+
+llist = LinkedList()
+llist.head = Node(1)
