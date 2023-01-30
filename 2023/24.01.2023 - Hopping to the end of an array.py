63c7e6c3dfc45d5e2b86666c4096961f549f6990
abelkent <abel.wbkent@gmail.com>
2023-01-30 16:32:54 +0000

    Update 24.01.2023 - Hopping to the end of an array.py
    -Implementation complete

--- 2023/24.01.2023 - Hopping to the end of an array.py
+++ 2023/24.01.2023 - Hopping to the end of an array.py
@@ -5,10 +5,19 @@ determine whether you can reach to the last index starting at index 0.
 For example, [2, 0, 1, 0] returns True while [1, 1, 0, 1] returns False.
 """
 
-"""def array_hopper(array):
+def array_hopper(array):
 
     position = int(0)
 
-    while array[position] = """
+    while position != len(array)-1:
 
-#UNFINISHED
+        if array[position] == 0:
+            return False
+        
+        else:
+            position += array[position]
+    
+    return True
+
+print(array_hopper([2, 0, 1, 0]))
+print(array_hopper([1, 1, 0, 1]))
