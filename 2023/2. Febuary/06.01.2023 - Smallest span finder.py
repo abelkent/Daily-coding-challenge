43e0df1195c624cc4c71cc7c0a0bc640c1897ed0
abelkent <abel.wbkent@gmail.com>
2023-02-07 16:26:55 +0000

    Update to 06
    - Started writing it, had a breakdown, bon apetit
    - Starting again

--- 2023/2. Febuary/06.01.2023 - Smallest span finder.py
+++ 2023/2. Febuary/06.01.2023 - Smallest span finder.py
@@ -5,5 +5,29 @@ If there are multiple smallest sets, return any of them.
 For example, given the intervals [0, 3], [2, 6], [3, 4], [6, 9], one set of numbers that covers all these intervals is {3, 6}.
 """
 
+import math
+
 def span_finder(intervals):
+    interval_ranges = []
+
+    lowest = math.inf
+    highest = int(0)
+
+    for interval in intervals:
+
+        if interval[0] < lowest:
+            lowest = interval[0]
+        if interval[1] > highest:
+            highest = interval[1]
+
+        local_range = [x for x in range(interval[0],interval[1]+1)]
+        interval_ranges.append(local_range)
     
+    print(interval_ranges)
+    print(lowest, highest)
+
+    all_encompasing = [x for x in range(lowest, highest+1)]
+    print(all_encompasing)
+
+
+span_finder([[0, 3], [2, 6], [3, 4], [6, 9]])
