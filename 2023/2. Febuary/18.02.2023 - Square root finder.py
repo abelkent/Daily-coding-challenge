e2696a27bd0cf95bf9421f098dc59a91f23dd0da
abelkent <abel.wbkent@gmail.com>
2023-02-20 16:50:02 +0000

    Create 18.02.2023 - Square root finder.py

---
+++ 2023/2. Febuary/18.02.2023 - Square root finder.py
@@ -0,0 +1,12 @@
+"""
+Given a real number n, find the square root of n. For example, given n = 9, return 3.
+"""
+
+import math
+
+def sqrt_cheatsy(n):
+    #Uses Python's math library to cheese the answer
+    return math.sqrt(n)
+
+
+
