61b09d51e2cdebdc307e7dfbf3039aadb954be57
abelkent <abel.wbkent@gmail.com>
2023-02-23 16:10:51 +0000

    Update 21.02.2023 - Custom timestamp tracker class.py
    - Second implementation

--- 2023/2. Febuary/21.02.2023 - Custom timestamp tracker class.py
+++ 2023/2. Febuary/21.02.2023 - Custom timestamp tracker class.py
@@ -4,6 +4,7 @@ Design and implement a HitCounter class that keeps track of requests (or hits). 
 record(timestamp): records a hit that happened at timestamp
 total(): returns the total number of hits recorded
 range(lower, upper): returns the number of hits that occurred between timestamps lower and upper (inclusive)
+
 Follow-up: What if our system has limited memory?
 """
 
@@ -34,9 +35,44 @@ class HitCounter():
         return hits_in_range
 
 #Tests
-counter = HitCounter()
+"""counter = HitCounter()
 counter.record(1)
 counter.record(3)
 counter.record(5)
 counter.total()
-counter.range(1,3)
+counter.range(1,3)"""
+
+#Implentation 2: More memory concerned
+
+#Memory is now bounded by x as number of unique timestamps rather than n of timestamps but still not functioning in fixed memory 
+class HitCounterConstrained():
+    def __init__(self):
+        self.data = dict()
+    
+    def record(self, timestamp):
+        if timestamp not in self.data.keys():
+            self.data[timestamp] = int(1)
+        else:
+            cache = self.data.get(timestamp)
+            cache += 1
+            self.data[timestamp] = cache
+    
+    def total(self):
+        print (sum(self.data.values()))
+        return sum(self.data.values())
+
+    def range(self, lower, upper):
+        hits_in_range = int(0)
+        for timestamp in self.data.keys():
+            if timestamp in range(lower, upper+1):
+                cache = self.data.get(timestamp)
+                hits_in_range += cache
+
+        print(hits_in_range)
+        return hits_in_range
+    
+counter = HitCounterConstrained()
+counter.record(1)
+counter.record(2)
+counter.record(1)
+counter.total()
