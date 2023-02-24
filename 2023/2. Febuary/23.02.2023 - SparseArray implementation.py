1e7de63d294ca4c8e0378cc795879b623900242b
abelkent <abel.wbkent@gmail.com>
2023-02-24 15:43:28 +0000

    Update 23.02.2023 - SparseArray implementation.py
    - Annotation update
    - Added functionality to return 0 in get() function

--- 2023/2. Febuary/23.02.2023 - SparseArray implementation.py
+++ 2023/2. Febuary/23.02.2023 - SparseArray implementation.py
@@ -8,15 +8,18 @@ set(i, val): updates index at i with val.
 get(i): gets the value at index i.
 """
 
+#Example "large" array
 example_array = list([0,0,0,0,0,3,0,1,0,5])
 print(example_array)
 
 class SparseArray():
     def __init__(self, arr, size):
-
+        
+        #Checks if size is smaller than given array
         if size < len(arr):
             raise IndexError("Proposed maximum length of array less than provided array")
 
+        #Initialises internal variables
         self.size = size
         self.data = list()
         for index in range(len(arr)):
@@ -24,11 +27,14 @@ class SparseArray():
             if entry != 0:
                 self.data.append([index,entry])
     
+    
     def set(self, i, val):
 
+        #Index error check
         if i > self.size:
             raise IndexError("Proposed index out of range")
 
+        #Updates value at given index, if value given is zero - deletes entry
         for entry in self.data:
             if entry[0] == i:
 
@@ -38,17 +44,27 @@ class SparseArray():
                    entry[1] = val
                 return
 
+        #If index given is not in SparseArray memory (ie, is zero - created)
+        #NOTE: current implementation does not order entries - simply appending new entries
+        #NOTE: Dict implementation may have faster retrieval speeds with inbuilt set/get functions
         if val != 0:
             self.data.append([i,val])
     
+
     def get(self, i):
+        #Index error check
         if i > self.size:
             raise IndexError("Proposed Index is out of scope of defined SparseArray size")
         
+        #Retrieves data from given index
         for entry in self.data:
             if entry[0] == i:
                 print(entry[1])
                 return (entry[1])
+        
+        #If not present, returns 0
+        print(0)
+        return (0)
 
 sparse = SparseArray(example_array, 100)
 print(sparse.data)
