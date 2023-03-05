c5e4329c7f59d84575672b9b354ddd29f9af469f
abelkent <abel.wbkent@gmail.com>
2023-03-05 17:53:46 +0000

    Update 05.03.2023 - Nearest large value in an array.py
    Annotation

--- 2023/3. March/05.03.2023 - Nearest large value in an array.py
+++ 2023/3. March/05.03.2023 - Nearest large value in an array.py
@@ -10,6 +10,7 @@ If the array at i doesn't have a nearest larger integer, then return null.
 
 def nearest_large_finder(array, starting_index):
     
+    #Retrieves value to beat from array and provided index
     value_to_beat = array[starting_index]
 
     #Left check
@@ -20,9 +21,9 @@ def nearest_large_finder(array, starting_index):
                 return (distance)
             else:
                 distance += 1
-    
         return (None)
     
+    #Right check
     def right_check(starting_index):
         distance = int(1)
         for index in range(starting_index+1, len(array),1):
@@ -30,18 +31,22 @@ def nearest_large_finder(array, starting_index):
                 return (distance)
             else:
                 distance += 1
-        
         return (None)
 
+    #Sets left and right as the minimal distance to a larger value on the left and right of the given index provided respectively
     left, right = left_check(starting_index), right_check(starting_index)
 
+    #If both are None, ie, there is no larger value - returns None
     if (left == None) and (right == None):
         return None
     
+    #If only one of them is None, return the other
     if left == None:
         return right
     elif right == None:
         return left
+    
+    #Otherwise returns minimum value of the two
     else:
         return min(left, right)
 
