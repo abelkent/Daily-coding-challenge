0f48c54879065b0bd8377d7be9542cdec939d507
abelkent <abel.wbkent@gmail.com>
2023-03-09 16:09:22 +0000

    Create 07.03.2023 - Binary tree pruning of zero subtrees.py
    - Seems to be working
    - Need to add binary tree representation function to double check

---
+++ 2023/3. March/07.03.2023 - Binary tree pruning of zero subtrees.py
@@ -0,0 +1,69 @@
+"""
+Given a binary tree where all nodes are either 0 or 1, prune the tree so that subtrees containing all 0s are removed.
+
+For example, given the following tree:
+
+   0
+  / \
+ 1   0
+    / \
+   1   0
+  / \
+ 0   0
+should be pruned to:
+
+   0
+  / \
+ 1   0
+    /
+   1
+We do not remove the tree at the root or its left child because it still has a 1 as a descendant.
+"""
+
+class Node:
+    def __init__(self, root, left = None, right = None):
+        self.root = root
+        self.left = left
+        self.right = right
+    
+    def check_if_all_zero(self):
+
+        if self.root == 0:
+            root_value = True
+        else:
+            root_value = False
+        
+        if self.left == None:
+            left_value = True
+        else:
+            left_value = self.left.check_if_all_zero()
+    
+        if self.right == None:
+            right_value = True
+        else:
+            right_value = self.right.check_if_all_zero()
+        
+        return (root_value and left_value and right_value)
+
+    def prune(self):
+        
+        if self.left == None:
+            pass
+        elif self.left.check_if_all_zero() == True:
+            self.left = None
+        else:
+            self.left.prune()
+        
+        if self.right == None:
+            pass
+        elif self.right.check_if_all_zero() == True:
+            self.right = None
+        else:
+            self.right.prune()
+
+test_node = Node(1,Node(0, Node(0)))
+print(test_node.check_if_all_zero())
+print(test_node.left.check_if_all_zero())
+print(test_node.left.root)
+test_node.prune()
+print(test_node.left.root)
