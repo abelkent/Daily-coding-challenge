a74b778181705946069e2326b60d5597760ef517
abelkent <abel.wbkent@gmail.com>
2023-03-14 17:16:49 +0000

    Update 11.03.2023 - Nearest k points.py
    "Hooray that works Im done"

--- 2023/3. March/11.03.2023 - Nearest k points.py
+++ 2023/3. March/11.03.2023 - Nearest k points.py
@@ -6,5 +6,24 @@ For example, given the list of points [(0, 0), (5, 4), (3, 1)], the central poin
 
 points = [(0, 0), (5, 4), (3, 1)]
 
+import math
+
 def nearest_k_points(points, centre, k):
+
+    distance_dict = dict()
+
+    for point in points:
+        distance_dict[math.dist(point, centre)] = point
     
+    distances = list(distance_dict.keys())
+    distances.sort()
+    
+    output = list()
+
+    for distance in distances[0:k]:
+        output.append(distance_dict.get(distance))
+
+    print(output)
+        
+
+print(nearest_k_points(points, (0,0), 2))
