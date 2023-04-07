909c2bb1899fc4f39754d2e3d2a335b59b54ac56
abelkent <abel.wbkent@gmail.com>
2023-04-07 15:22:28 +0100

    Update 17.03.2023 - Majority element of a list.py
    - Annotation update

--- 2023/3. March/17.03.2023 - Majority element of a list.py
+++ 2023/3. March/17.03.2023 - Majority element of a list.py
@@ -10,7 +10,10 @@ For example, given [1, 2, 1, 1, 3, 4, 0], return 1.
 
 def majority_finder(array):
 
+    #Initialises a blank dict object to store occurences
     element_dict = dict()
+
+    #Iterates through array and builds dict of occurences
     for element in array:
         if element in element_dict.keys():
             element_dict[element] += 1
@@ -18,16 +21,19 @@ def majority_finder(array):
         else:
             element_dict[element] = int(1)
 
-    print(element_dict)
+    #print(element_dict)
 
+    #Initialises objects to track most common occurence
     largest = int(0)
     output = None
     
+    #Determines most common element
     for element in element_dict.keys():
         if element_dict.get(element) > largest:
             output = element
             largest = element_dict.get(element)
     
+    #Prints and returns most common element
     print(output)
     return output
 
