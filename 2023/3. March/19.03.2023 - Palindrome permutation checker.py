1f13eb1040e22935118b82312ae00abd7b8b9ede
abelkent <abel.wbkent@gmail.com>
2023-04-07 16:45:20 +0100

    Update 19.03.2023 - Palindrome permutation checker.py
    - Annotation update

--- 2023/3. March/19.03.2023 - Palindrome permutation checker.py
+++ 2023/3. March/19.03.2023 - Palindrome permutation checker.py
@@ -5,32 +5,38 @@ For example, carrace should return true, since it can be rearranged to form race
 which is a palindrome. daily should return false, since there's no rearrangement that can form a palindrome.
 """
 
+#Definition of function to check if a number is even (for later use)
 def is_even(x):
     return (x%2 == 0)
 
 
 def palindrome_permutation_check(word):
+
     word = list(word)
     letter_dict = dict()
+    #Builds dict of letter occurences in word
     for letter in word:
         if letter not in letter_dict.keys():
             letter_dict[letter] = 1
         else:
             letter_dict[letter] += 1
     
+    #Retrieves number of occurences of letters
     letter_population = list(letter_dict.values())
 
+    #Counts number of odd number counts of letters
     odds = int(0)
     for count in letter_population:
         if not is_even(count):
             odds += 1
     
+    #If no odd letters, return True
     if odds == 0:
         return True
-    elif ((odds >= 1) and (is_even(len(word)))):
-        return True
+    #If 1 occurence of an odd letter, and length of word is not even, return True
     elif ((odds == 1) and not(is_even(len(word)))):
         return True
+    #Otherwise return False
     else:
         return False
     
