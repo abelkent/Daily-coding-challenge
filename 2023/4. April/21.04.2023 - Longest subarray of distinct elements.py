ed0b65ee02329782309a424e756ae2c66fe2d3bb
abelkent <abel.wbkent@gmail.com>
2023-04-28 15:41:28 +0100

    Update 21.04.2023 - Longest subarray of distinct elements.py
    - Annotation update

--- 2023/4. April/21.04.2023 - Longest subarray of distinct elements.py
+++ 2023/4. April/21.04.2023 - Longest subarray of distinct elements.py
@@ -7,18 +7,24 @@ For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], return 5 as the longest s
 example_array = [5, 1, 3, 5, 2, 3, 4, 1]
 
 def subarray_finder(array):
-
+    
+    #Initialises empty lists for maximum and current local subarray
     maximum_array = list()
-
     local_subarray = list()
+    
+    #Iterates through all elements of input array
     for element in array:
+        #Appends each element to local_subarray
         local_subarray.append(element)
         
+        #Generates set of local_subarray for purposes of duplicate detection
         local_subset = set(local_subarray)
+        #If a duplicate element is present (as detected by difference in set and array length difference) checks if local is greater than maximum and supplants it if True
         if len(local_subarray) != len(local_subset):
             if len(maximum_array) < len(local_subarray):
                 maximum_array = local_subarray
             
+            #While a duplicate is present in array, pops off first element until no longer True
             while True:
                 local_subarray.pop(0)
                 local_subset = set(local_subarray)
@@ -26,6 +32,7 @@ def subarray_finder(array):
                 if len(local_subarray) == len(local_subset):
                     break
     
+    #Returns maximum subarray
     return maximum_array
 
 print(subarray_finder(example_array))
